e737162695ba6362010b4f86b8ef12376485c958
nitraxx07-source <nitraxx07@gmail.com>
2026-05-14 13:10:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
 import { registerRootComponent } from 'expo';
 import App from './App';
 
-// PARCHE CRÍTICO: Engañamos al sistema para que no busque las herramientas rotas
-global.__expo_device_log_disabled = true;
-
-// Forzamos a que NO use las herramientas de desarrollo de React
-if (__DEV__) {
-  const { NativeModules } = require('react-native');
-  if (NativeModules.DevSettings) {
-    NativeModules.DevSettings.setIsDebuggingRemotely(false);
-  }
-}
-
 registerRootComponent(App);
