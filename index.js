11f8ed549e182e27e301007386ed1807aa4554c5
nitraxx07-source <nitraxx07@gmail.com>
2026-05-14 11:45:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 import { registerRootComponent } from 'expo';
 import App from './App';
 
-// Esto le dice a Android qué archivo cargar primero
+// PARCHE CRÍTICO: Engañamos al sistema para que no busque las herramientas rotas
+global.__expo_device_log_disabled = true;
+
+// Forzamos a que NO use las herramientas de desarrollo de React
+if (__DEV__) {
+  const { NativeModules } = require('react-native');
+  if (NativeModules.DevSettings) {
+    NativeModules.DevSettings.setIsDebuggingRemotely(false);
+  }
+}
+
 registerRootComponent(App);
