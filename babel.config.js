d07848e4200affa3f27a4ee22f5b41073cf6adaa
nitraxx07-source <nitraxx07@gmail.com>
2026-05-14 11:11:09 -0500

    Clear plugins array in babel.config.js
    Removed react-native-reanimated plugin to fix Worklets error.

--- babel.config.js
+++ babel.config.js
@@ -2,8 +2,6 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      'react-native-reanimated/plugin',
-    ],
+    plugins: [], // Dejamos esto vacío para eliminar el error de Worklets
   };
 };
