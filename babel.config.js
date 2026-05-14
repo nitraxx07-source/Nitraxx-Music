40146acb40e0b09972eaaa340d2bb936f8813803
nitraxx07-source <nitraxx07@gmail.com>
2026-05-14 11:29:14 -0500

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,6 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [], // <--- ASEGÚRATE DE QUE ESTO ESTÉ VACÍO
+    plugins: ['react-native-reanimated/plugin'],
   };
 };
